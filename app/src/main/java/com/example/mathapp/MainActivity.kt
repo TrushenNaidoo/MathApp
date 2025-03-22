79291d2c82d166a9289dcaa00cd42fb4ced4174e
TrushenNaidoo <ST10479724@vcconnect.edu.za>
2025-03-22 23:34:58 +0200

    MathApp

---
+++ app/src/main/java/com/example/mathapp/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.mathapp
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+
+        val number1 =  findViewById<EditText>(R.id.number1)
+        val number2 = findViewById<EditText>(R.id.number2)
+        val button1 = findViewById<Button>(R.id.button1)
+        val txtView2 = findViewById<TextView>(R.id.txtView)
+
+        button1.setOnClickListener {
+            val num1 = number1.text.toString().toInt()
+            val num2 = number2.text.toString().toInt()
+
+            val sum = num1 + num2
+            txtView2.text = sum.toString()
+        }
+    }
+}
